9718052fe061a6ca3b8c414c198d34e9da528051
Justin Stahlman <jstahlman@mac.com>
2016-03-12 18:31:47 -0500

    modify grass

--- src/lib/game/entities/lapin.js
+++ src/lib/game/entities/lapin.js
@@ -10,6 +10,20 @@ ig.module(
 
         size: { x: 48, y: 48 },
         collides: ig.Entity.COLLIDES.PASSIVE,
+        maxVel: {
+			x: 275,
+			y: 300
+		},
+		accelGround: 500,
+		accelAir: 300,
+		speed: {current:0,normal:300,ladder:600},
+        friction: {x:400,y:600},
+
+        type: ig.Entity.TYPE.A, // Player friendly group
+		checkAgainst: ig.Entity.TYPE.B,
+        collides: ig.Entity.COLLIDES.PASSIVE,
+
+		name:"player",
 
 
         animSheet: new ig.AnimationSheet('media/lapin-sprite.png', 48, 48),
@@ -18,32 +32,54 @@ ig.module(
 
             this.parent(x, y, settings);
 
+			ig.game.player = this;
             this.addAnim('idle', 1, [0]);
 	        this.addAnim('run', 0.1, [1,2,3,4,5,2]);
 
-			this.currentAnim = this.anims.run;
+
 
         },
 
         update: function() {
 
+
 			// Handle user input; move left or right
 			var accel = this.standing ? this.accelGround : this.accelAir;
 			if( ig.input.state('left')) {
-				if (this.accel.x > 0) this.vel.x *=0.05; // prevents sliding as if on ice when changing direction
+				if (this.accel.x > 0) this.vel.x *=0.7; // prevents sliding as if on ice when changing direction
 				this.accel.x = -accel;
 				this.flip = true;
 
 			}
 			else if( ig.input.state('right')) {
-				if (this.accel.x < 0) this.vel.x *=0.05; // prevents sliding as if on ice when changing direction
+				if (this.accel.x < 0) this.vel.x *=0.7; // prevents sliding as if on ice when changing direction
 				this.accel.x = accel;
 				this.flip = false;
 			}
+			else if( ig.input.state('up')) {
+				if (this.accel.y > 0) this.vel.y *=0.7; // prevents sliding as if on ice when changing direction
+				this.accel.y = -accel;
+				//this.flip = true;
+
+			}
+			else if( ig.input.state('down')) {
+				if (this.accel.y < 0) this.vel.y *=0.7; // prevents sliding as if on ice when changing direction
+				this.accel.y = accel;
+				//this.flip = false;
+			}
 			else {
 				this.accel.x = 0;
+				this.accel.y = 0;
 			}
-            this.parent();
+ig.show('vel.x',this.vel.x);
+ig.show('vel.y',this.vel.y);
+
+			if (this.vel.x == 0 && this.vel.y == 0) this.currentAnim = this.anims.idle;
+			else this.currentAnim = this.anims.run;
+
+			this.currentAnim.flip.x = this.flip;
+
+			this.parent();
 
         }
     });
--- src/lib/game/main.js
+++ src/lib/game/main.js
@@ -33,6 +33,14 @@ MyGame = ig.Game.extend({
 
 
 	update: function() {
+
+		// camera follow player
+		if( ig.game.player ) {
+			this.screen.x = ig.game.player.pos.x - ig.system.width/2;
+			this.screen.y = ig.game.player.pos.y - ig.system.height/2;
+		}
+
+
 		// Update all entities and backgroundMaps
 		this.parent();
 
@@ -53,8 +61,40 @@ MyGame = ig.Game.extend({
 });
 
 
-// Start the Game with 60fps, a resolution of 320x240, scaled
-// up by a factor of 2
-ig.main( '#canvas', MyGame, 60, 320, 240, 2 );
+	// If our screen is smaller than 640px in width (that's CSS pixels), we scale the
+	// internal resolution of the canvas by 2. This gives us a larger viewport and
+	// also essentially enables retina resolution on the iPhone and other devices
+	// with small screens.
+	//var scale = (window.innerWidth < 640) ? 2 : 1;
+	var scale = (window.innerWidth < 640) ? 1 : 0.5; // Modif to make pixelated
+	// We want to run the game in "fullscreen", so let's use the window's size
+	// directly as the canvas' style size.
+	var canvas = document.getElementById('canvas');
+	canvas.style.width = window.innerWidth + 'px';
+	canvas.style.height = window.innerHeight + 'px';
+	// Listen to the window's 'resize' event and set the canvas' size each time
+	// it changes.
+	window.addEventListener('resize', function() {
+		// If the game hasn't started yet, there's nothing to do here
+		if (!ig.system) {
+			return;
+		}
+		// Resize the canvas style and tell Impact to resize the canvas itself;
+		canvas.style.width = window.innerWidth + 'px';
+		canvas.style.height = window.innerHeight + 'px';
+		ig.system.resize(window.innerWidth * scale, window.innerHeight * scale);
+		// Re-center the camera - it's dependend on the screen size.
+		if (ig.game && ig.game.setupCamera) {
+			ig.game.setupCamera();
+		}
+		// Also repositon the touch buttons, if we have any
+		if (window.myTouchButtons) {
+			window.myTouchButtons.align();
+		}
+	}, false);
 
+//ig.main( '#canvas', MyGame, 60, 320, 240, 2 );
+	var width = window.innerWidth * scale,
+		height = window.innerHeight * scale;
+	ig.main('#canvas', MyGame, 60, width, height, 2);
 });
